c62f8606d250c7c98af06a366beb32e1a39defd6
matheusgabbe <105052059+matheusgabbe@users.noreply.github.com>
2024-03-15 10:48:44 -0300

    HBYG

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
+const contadores = document.querySelectorAll(".contador");
+
 
 for (let i = 0; i < botoes.length; i++) {
     botoes[i].onclick = function () {
@@ -13,4 +15,10 @@ for (let i = 0; i < botoes.length; i++) {
         textos[i].classList.add("ativo");
     }
 }
+
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
+let tempoAtual = new Date();
+
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
 
